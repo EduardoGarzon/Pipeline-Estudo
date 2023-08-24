7a9066a688e3ca3c9c07fef78a9f704d62e99150
EduardoGarzon <luizeduardo3131@gmail.com>
2023-08-24 09:07:55 -0300

    Incluindo Arquivos.

---
+++ ProdEscLP02/prodEscLP02.c
@@ -0,0 +1,73 @@
+/*
+    Para fazer sentido esta prática, o código deve ser compilado sem otimização
+    compilação simples sem otimização (-O0):
+        gcc -O0 prodEscOR.c -o prodEscOR.x
+    para profiler (-gp):
+        gcc -O0 -pg prodEscOR.c -o prodEscOR.x
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 200000000 // altere para +- 50% da sua memória RAM (N*8*2)
+
+double prodEscalar(double *v1, double *v2)
+{
+    double sum = 0.0;
+    double sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
+    for (int i = 0; i < N; i += 4)
+    {
+        sum1 += v1[i] * v2[i];
+        sum2 += v1[i + 1] * v2[i + 1];
+        sum3 += v1[i + 2] * v2[i + 2];
+        sum4 += v1[i + 3] * v2[i + 3];
+    }
+    sum += (sum1 + sum2 + sum3 + sum4);
+
+    return sum;
+}
+
+void solucao(double n)
+{
+    printf("Produto Escalar: %0.2f\n", n);
+}
+
+double *inicializarVetor()
+{
+    double *v;
+    v = (double *)calloc(N, sizeof(double));
+    for (int i = 0; i < N; i += 4)
+    {
+        v[i] = i;
+        v[i + 1] = i + 1;
+        v[i + 2] = i + 2;
+        v[i + 3] = i + 3;
+    }
+    return v;
+}
+
+int main()
+{
+    double vsum = 0.0;
+    double *v1, *v2;
+
+    // printf("memória estimada: %0.2f GiB\n", ((((N*8.0*2.0) / 1024)/1024)/1024));
+
+    v1 = inicializarVetor();
+    v2 = inicializarVetor();
+
+    if (v1 == NULL || v2 == NULL)
+    {
+        printf("calloc falhou para %u double\n", N);
+        return -1;
+    }
+
+    vsum = prodEscalar(v1, v2);
+
+    solucao(vsum);
+
+    free(v1);
+    free(v2);
+
+    return 0;
+}
